0be901879bf7e49079d4244143de4bdc640062e3
DuoChen-317 <dxc830@case.edu>
2025-11-02 16:19:28 -0500

    finish the pipeline

--- models_eval/eval_qwn_1.5b.py
+++ models_eval/eval_qwn_1.5b.py
@@ -1,9 +1,9 @@
 from vllm import LLM, SamplingParams
 from detoxify import Detoxify
-
+from collections import defaultdict
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from dataset.load_xnil import load_idassigned_dataset
+from dataset.load_xnli import load_idassigned_dataset
 from emotion_eval.emotional_eval import bias_evaluation
 
 
@@ -12,7 +12,8 @@ DATASET_SUBSET = "all_languages"
 MODEL = "Qwen/Qwen2.5-1.5B-Instruct"
 SUPPORTED_LANGUAGES = ["en", "fr", "ru", "es", "tr"]
 SAMPLE_NUMBER = 5
-final_results = []
+text_results = []
+bias_results = []  # to store bias for each sample
 
 #load dataset
 ds = load_idassigned_dataset(DATASET, DATASET_SUBSET)
@@ -24,6 +25,8 @@ sampling = SamplingParams(
     top_p=0.9,
     max_tokens=256,
 )
+# load detoxify 
+detox = Detoxify('multilingual')
 
 
 for lang in SUPPORTED_LANGUAGES:
@@ -39,24 +42,39 @@ for lang in SUPPORTED_LANGUAGES:
     print("Evaluating toxicity scores...")
     for i in range(SAMPLE_NUMBER):
         gen_text = outputs[i].outputs[0].text.strip()
-        toxicity_score = Detoxify('multilingual').predict(gen_text)["toxicity"]
-        final_results.append({
+        toxicity_score = detox.predict(gen_text)["toxicity"]
+        text_results.append({
             "id": ds[i]["id"],
             "lang": lang,
-            "input": ds['premise'][i][lang],
+            "input": ds[i]['premise'][[lang],
             "output": gen_text,
             "toxicity": toxicity_score
         })
 
-## EVALUATE BIAS
-print(" Evaluating bias metrics...")
+# Group all toxicity scores by sample ID
+toxicity_by_id = defaultdict(list)
+for result in text_results:
+    sample_id = result["id"]
+    toxicity_by_id[sample_id].append(result["toxicity"])
+for sample_id, scores in toxicity_by_id.items():
+    if len(scores) > 1:  # only meaningful if the sample exists in multiple languages
+        bias_values = bias_evaluation(scores) # dict
+        bias_results.append({
+            "id": sample_id,
+            "bias_values":bias_values
+        })
+
+# save the result
+import json
+
+with open("bias_results.json", "w", encoding="utf-8") as f:
+    json.dump(bias_results, f, indent=4, ensure_ascii=False)
+with open("text_results.json", "w", encoding="utf-8") as f:
+    json.dump(text_results, f, indent=4, ensure_ascii=False)
+
+print("✅ Saved bias results to bias_results.json")
+print("✅ Saved text results to text_results.json")
 
 
 
-# PRINT RESULTS
-for res in final_results:
-    print("Language:", res["lang"])
-    print("Input Text:", res["input"])
-    print("Generated Text:", res["output"])
-    print("Toxicity Score:", res["toxicity"])
-    print("-" * 50)
+
