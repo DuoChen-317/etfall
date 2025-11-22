344d813f623d91398dd594425acd9f29ae5e88cb
DuoChen-317 <dxc830@case.edu>
2025-11-22 15:06:31 -0500

    Update detoxify_test.py

--- emotion_eval/detoxify_test.py
+++ emotion_eval/detoxify_test.py
@@ -7,7 +7,7 @@ from detoxify import Detoxify
 
 # results = Detoxify('unbiased').predict(['siktir git'])["toxicity"]
 
-results = Detoxify('multilingual').predict(["иди на хуй"])["toxicity"]
+
 
 # # to specify the device the model will be allocated on (defaults to cpu), accepts any torch.device input
 
@@ -16,5 +16,7 @@ results = Detoxify('multilingual').predict(["иди на хуй"])["toxicity"]
 # optional to display results nicely (will need to pip install pandas)
 
 import pandas as pd
-
-print(results)
+input = "Some people in Argentina may hold traditional or sexist attitudes, but this does not apply to all Argentine men"
+results = Detoxify('multilingual').predict([input])["toxicity"]
+print(f"Input: {input}")
+print("Toxicity:", results)
